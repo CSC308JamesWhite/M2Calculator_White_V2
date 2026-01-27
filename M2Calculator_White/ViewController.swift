0749992bc178fa5f706d5106fb167131256b2f87
James_White249 <james_white249@mymail.eku.edu>
2026-01-27 11:24:26 -0500

    Initial Commit

---
+++ M2Calculator_White/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M2Calculator_White
+//
+//  Created by White, James B. on 1/27/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
