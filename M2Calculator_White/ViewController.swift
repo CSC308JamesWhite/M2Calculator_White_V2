f9df44a48cf88ca85e57fc263907236cb8e6396c
james_white249 <james_white249@mymail.eku.edu>
2026-04-09 12:01:43 -0400

    april9thclass

--- M2Calculator_White/ViewController.swift
+++ M2Calculator_White/ViewController.swift
@@ -39,31 +39,66 @@ class ViewController: UIViewController {
     }
     @IBOutlet weak var resultLabel: UITextField!
     @IBAction func resultButton(_ sender: Any) {
-        let op1 = Int(operand1TextField.text!)!
-        let op2 = Int(operand2TextField.text!)!
-        let op = selectOperator.title(for: .normal)
-        
-        if op == "+" {
-            resultLabel.text = "\(op1 + op2)"
-        } else if op == "-" {
-            resultLabel.text = "\(op1 - op2)"
-        } else if op == "*" {
-            resultLabel.text = "\(op1 * op2)"
-        } else if op == "/" {
-            resultLabel.text = "\(op1 / op2)"
-        } else {
-            print("Invalid Operator")
+        func showAlert(message: String){
+                let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default)
+                alert.addAction(okAction)
+                present(alert, animated: true)
+            }
+        //        let op1 = Int(operand1TextField.text!)!
+        //        let op2 = Int(operand2TextField.text!)!
+        //        let op = selectOperator.title(for: .normal)!
+                guard let op1 = operand1TextField.text, let a = Int(op1)else{
+                    showAlert(message: "Please enter a valid number.")
+                    return
+                }
+                guard let op2 = operand2TextField.text, let b = Int(op2)else{
+                    showAlert(message: "Please enter a valid number.")
+                    return
+                }
+                guard let op = selectOperator.title(for: .normal), op != "?" else{
+                    showAlert(message: "Please select an operator.")
+                    return
+                }
+                
+                var result: Int? = nil
+                switch op {
+                case "+":
+                    result = a + b
+                case "-":
+                    result = a - b
+                case "*":
+                    result = a * b
+                case "/":
+                    result = a / b
+                default:
+                    print("Please select an operator.")
+                }
+                guard let result = result else{
+                    return
+                }
+                resultLabel.text = "\(result)"
+    
+                operand1TextField.resignFirstResponder()
+                operand2TextField.resignFirstResponder()
+            }
+            
+            
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                operand1TextField.delegate = self
+                operand2TextField.delegate = self
+                // Do any additional setup after loading the view.
+            }
+
+
         }
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
-
+extension ViewController: UITextFieldDelegate{
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard !string.isEmpty else { return true }
+        guard let _ = Int(string) else{ return false }
+        
+        return true
+    }
 }
-
-//command+B : build your project
-//command+R : run your project
